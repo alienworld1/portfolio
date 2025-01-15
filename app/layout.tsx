3fc758e52d9e37f2ba435f50115b61294900f6a1
alienworld <69631697+alienworld1@users.noreply.github.com>
2025-01-15 14:57:59 +0000

    Add header and not found page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 import NightSky from './components/night-sky';
+import Portfolio from './components/portfolio';
 
 export const metadata: Metadata = {
   title: 'Personal Homepage',
@@ -17,7 +18,11 @@ export default function RootLayout({
     <html lang="en">
       <body className="font-body relative h-screen">
         <NightSky />
-        {children}
+        <div className="absolute inset-0 flex flex-col items-center justify-center h-screen">
+          <Portfolio>
+            {children}
+          </Portfolio>
+        </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
-import Portfolio from './components/portfolio';
-
 export default function Page() {
   return (
-    <div className="absolute inset-0 flex flex-col items-center justify-center h-screen">
-      <Portfolio>
-        <h1 className="text-4xl font-bold text-white">Hello, World!</h1>
-        <p className="text-lg text-white">Welcome to my personal homepage.</p>
-      </Portfolio>
-    </div>
+    <>
+      <h1 className="text-4xl font-bold text-white">Hello, World!</h1>
+      <p className="text-lg text-white">Welcome to my personal homepage.</p>
+    </>
   );
 }
