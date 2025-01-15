0c00de63f44b951aa9e7dd3ad965737887e43347
alienworld <69631697+alienworld1@users.noreply.github.com>
2025-01-15 16:15:35 +0000

    Fix linting errors
    Escape the needed characters

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ export default function Page() {
         Full Stack Developer
       </h1>
       <p className="text-lg text-slate-100">
-        Good to see you! I'm Aditya, a software developer (and a college
-        student), who can create neat looking web and desktop apps. I can't
+        Good to see you! I&apos;m Aditya, a software developer (and a college
+        student), who can create neat looking web and desktop apps. I can&apos;t
         stand the fact that there are so many things that I am unable to do.
-        That fact may be true for the rest of my life, but it won't stop me from
-        learning as much as I can.
+        That fact may be true for the rest of my life, but it won&apos;t stop me
+        from learning as much as I can.
       </p>
       <h2 className="text-2xl font-bold text-slate-100">What I do</h2>
       <ul className="text-lg text-slate-100">
@@ -19,8 +19,8 @@ export default function Page() {
           C#).
         </li>
         <li>
-          🌐 I develop web applications, using TypeScript. I've developed MERN
-          stack apps, and Next.js apps too.
+          🌐 I develop web applications, using TypeScript. I&apos;ve developed
+          MERN stack apps, and Next.js apps too.
         </li>
         <li>🧠 I am currently studying artificial neural networks.</li>
       </ul>
